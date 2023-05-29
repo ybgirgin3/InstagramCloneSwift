130b479690ceb196c679404c472a00ce32bea4fd
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-05-29 21:01:28 +0300

    Initial Commit

---
+++ instagramTutorial/instagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramTutorialApp.swift
+//  instagramTutorial
+//
+//  Created by Yusuf Berkay Girgin on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct instagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
